a4468e706d792fbf75c96269d7413e05facbe09d
Mohammad Sleibi <msalibi83@gmail.com>
2019-08-08 23:02:03 +0200

    feat: Add new Templates and Statics
     add new html files to template
    
    and delete some files and create a static file for CSS

--- application.py
+++ application.py
@@ -3,11 +3,11 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Categories, Items
+from database_setup import Base, Categories, Properties
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///catalogapp.db')
+engine = create_engine('sqlite:///carcatalogapp.db', connect_args={'check_same_thread': False}, echo=True)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -18,47 +18,84 @@ session = DBSession()
 @app.route('/categories/')
 def categoriesMenu():
     category = session.query(Categories).all()
-    
-    
-    return render_template('categories.html',category=category)
+        
+    return render_template('categoriesMenu.html',category=category)
 
-#Show category items
+# CREATE NEW CATEGORY
+@app.route('/categories/new', methods=['GET', 'POST'])
+def newCategory():
+    if request.method == 'POST':
+        newCat = Categories(name=request.form['name'])
+        session.add(newCat)
+        session.commit()
+        flash("new category " + newCat.name  + " created!")
+        return redirect(url_for('categoriesMenu'))
+    else:
+	return render_template('newCategory.html')
+        
+
+# EDIT EXISTING CATEGORY
+@app.route('/categories/<int:category_id>/edit', methods=['GET', 'POST'])
+def editCategory(category_id):
+    
+    editCat = session.query(Categories).filter_by(id=category_id).one()
+    if request.method =='POST':
+        editCat.name = request.form['name']
+        session.add(editCat)
+        session.commit()
+        flash("Category has been edited")
+        return redirect(url_for('categoriesMenu'))
+    else:
+        return render_template('editCategory.html',category = editCat,category_id=editCat.id)
+    
+# DELETE EXISTING CATEGORY
+@app.route('/categories/<int:category_id>/delete',methods=['GET', 'POST'])
+def deleteCategory(category_id):
+    deleteCat = session.query(Categories).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        session.delete(deleteCat)
+        session.commit()
+        flash(deleteCat.name + " has been deleted")
+        return redirect(url_for('categoriesMenu'))
+    else:
+        return render_template('deleteCategory.html', deleteCategory=deleteCat)
+
+
+#Show category properties
 @app.route('/category/<int:category_id>/')
-@app.route('/category/<int:category_id>/items')
-def categoryItems(category_id):
-
-
+@app.route('/category/<int:category_id>/properties')
+def categoryProperties(category_id):
+    CarOne = session.query(Categories).filter_by(id=category_id).one()
+    properties = session.query(Properties).filter_by(categories_id=category_id)
     
-    return render_template('items.html')
+    return render_template('propertiesMenu.html',car = CarOne,properties = properties, category_id = category_id)
 
 
+#Show property description
+@app.route('/category/<int:category_id>/properties/<int:property_id>/')
+def showPropertyDescription(category_id, property_id):
 
-#Show item description
-@app.route('/category/<int:category_id>/items/<int:item_id>/')
-def showItemDescription(category_id, item_id):
-
-    
-    return render_template('itemDescription.html')
+    return render_template('propertyDescription.html')
 
 
-# Add item
-@app.route('/category/<int:category_id>/items/new')
-def newCategoryItem(category_id):
+# Add property
+@app.route('/category/<int:category_id>/properties/new')
+def newCategoryProperty(category_id):
 
-    return render_template('newItem.html')
+    return render_template('newProperty.html')
 
-# Edit item
-@app.route('/category/<int:category_id>/items/<int:item_id>/edit')
-def editCategoryItem(category_id, item_id):
+# Edit property
+@app.route('/category/<int:category_id>/properties/<int:property_id>/edit')
+def editCategoryProperty(category_id, property_id):
 
-    return render_template('editItem.html')
+    return render_template('editProperty.html')
 
 
-# Delete item
-@app.route('/category/<int:category_id>/items/<int:item_id>/delete')
-def deleteCategoryItem(category_id, item_id):
+# Delete property
+@app.route('/category/<int:category_id>/properties/<int:property_id>/delete')
+def deleteCategoryProperty(category_id, property_id):
 
-    return render_template('deleteItem.html')
+    return render_template('deleteProperty.html')
 
 
 if __name__ == '__main__':
--- database_setup.py
+++ database_setup.py
@@ -8,19 +8,24 @@ from sqlalchemy import create_engine
 Base = declarative_base()
  
 class Categories(Base):
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
    
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
  
-class Items(Base):
-    __tablename__ = 'items'
+class Properties(Base):
+    __tablename__ = 'properties'
 
 
-    name =Column(String(80), nullable = False)
+    model =Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     description = Column(String(250))
-    category_id = Column(Integer,ForeignKey('category.id'))
+    speed = Column(String(50))
+    carEngine = Column(String(50),nullable = False)
+    manufacture = Column(String(50),nullable = False)
+    carClass = Column(String(50),nullable = False)
+    photo = Column(String(250),nullable = True)
+    categories_id = Column(Integer,ForeignKey('categories.id'))
     category = relationship(Categories)
 
 #We added this serialize function to be able to send JSON objects in a serializable format
@@ -28,12 +33,16 @@ class Items(Base):
     def serialize(self):
        #Returns object data in easily serializeable format
        return {
-           'name'         : self.name,
+           'model'        : self.name,
            'description'  : self.description,
-           'id'         : self.id,
+           'id'           : self.id,
+           'speed'        : self.speed,
+           'carEngine'    : self.carEngine,
+           'manufacture'  : self.manufacture,
+           'carClass'     : self.carClass,
            }
 
-engine = create_engine('sqlite:///catalogapp.db')
+engine = create_engine('sqlite:///carcatalogapp.db')
  
 
 Base.metadata.create_all(engine)
