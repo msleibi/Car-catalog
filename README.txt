5cb45a6d71ea6c3a04626b0c50a5d7056dc157b9
Mohammad Sleibi <msalibi83@gmail.com>
2019-08-13 14:59:09 +0200

    feat: add CRUD Functions
     add all CRUD functions (Create, Read, Update, Delete)

--- README.txt
+++ README.txt
@@ -2,6 +2,4 @@ Place your catalog project in this directory.
 
 
 
-How to connect ot PostgreSql using SQLAlchemy
 
-https://www.learndatasci.com/tutorials/using-databases-python-postgres-sqlalchemy-and-alembic/
--- application.py
+++ application.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template, request, redirect, url_for, flash
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Categories, Properties
+from database_setup import Base, Categories, Items
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///carcatalogapp.db', connect_args={'check_same_thread': False}, echo=True)
+engine = create_engine('sqlite:///catalogapp.db', connect_args={'check_same_thread': False}, echo=True)
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -18,17 +18,19 @@ session = DBSession()
 @app.route('/categories/')
 def categoriesMenu():
     category = session.query(Categories).all()
+    lastItems = session.query(Items).order_by(desc("createdate")).limit(10)
         
-    return render_template('categoriesMenu.html',category=category)
+    return render_template('categoriesMenu.html',category=category,lastItems = lastItems)
 
 # CREATE NEW CATEGORY
 @app.route('/categories/new', methods=['GET', 'POST'])
+
 def newCategory():
     if request.method == 'POST':
         newCat = Categories(name=request.form['name'])
         session.add(newCat)
         session.commit()
-        flash("new category " + newCat.name  + " created!")
+        flash("new category %s has been created!" % (newCat.name))
         return redirect(url_for('categoriesMenu'))
     else:
 	return render_template('newCategory.html')
@@ -43,10 +45,10 @@ def editCategory(category_id):
         editCat.name = request.form['name']
         session.add(editCat)
         session.commit()
-        flash("Category has been edited")
-        return redirect(url_for('categoriesMenu'))
+        flash( '%s has been edited' % (editCat.name))
+        return redirect(url_for('categoryItems',category_id=editCat.id))
     else:
-        return render_template('editCategory.html',category = editCat,category_id=editCat.id)
+        return render_template('editCategory.html',categ = editCat,category_id=editCat.id)
     
 # DELETE EXISTING CATEGORY
 @app.route('/categories/<int:category_id>/delete',methods=['GET', 'POST'])
@@ -55,47 +57,65 @@ def deleteCategory(category_id):
     if request.method == 'POST':
         session.delete(deleteCat)
         session.commit()
-        flash(deleteCat.name + " has been deleted")
+        flash(' %s has been deleted' % (deleteCat.name))
         return redirect(url_for('categoriesMenu'))
     else:
         return render_template('deleteCategory.html', deleteCategory=deleteCat)
 
 
-#Show category properties
+#Show category items
 @app.route('/category/<int:category_id>/')
-@app.route('/category/<int:category_id>/properties')
-def categoryProperties(category_id):
-    CarOne = session.query(Categories).filter_by(id=category_id).one()
-    properties = session.query(Properties).filter_by(categories_id=category_id)
+@app.route('/category/<int:category_id>/items')
+def categoryItems(category_id):
+    CatOne = session.query(Categories).filter_by(id=category_id).one()
+    items = session.query(Items).filter_by(categories_id=category_id)
     
-    return render_template('propertiesMenu.html',car = CarOne,properties = properties, category_id = category_id)
+    return render_template('itemsMenu.html',categ = CatOne,items=items)
 
 
-#Show property description
-@app.route('/category/<int:category_id>/properties/<int:property_id>/')
-def showPropertyDescription(category_id, property_id):
+# Add item
+@app.route('/category/<int:category_id>/items/new',methods=['GET','POST'])
+def newCategoryItem(category_id):
+    CatOne = session.query(Categories).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        newItem = Items(name = request.form['name'], description = request.form['description'], price = request.form['price'],manufacture = request.form['manufacture'] , categories_id = category_id)
+        session.add(newItem)
+        session.commit()
+        flash('New Menu %s Item Successfully created' % (newItem.name))
+        return redirect(url_for('categoryItems', category_id = CatOne.id ))
+    else:
+        return render_template('newItem.html',category_id = CatOne.id,categ = CatOne)
 
-    return render_template('propertyDescription.html')
+# Edit item
+@app.route('/category/<int:category_id>/items/<int:item_id>/edit',methods=['GET','POST'])
+def editCategoryItem(category_id, item_id):
+    CatOne = session.query(Categories).filter_by(id=category_id).one()
+    editedItem = session.query(Items).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        editedItem.name = request.form['name']
+        editedItem.description = request.form['description']
+        editedItem.price = request.form['price']
+        editedItem.manufacture = request.form['manufacture'] 
+        session.add(editedItem)
+        session.commit()
+        flash('Menu %s Item Successfully edited' % (editedItem.name))
+        return redirect(url_for('categoryItems', category_id = CatOne.id))
+    else:
+        return render_template('editItem.html',categ = CatOne,item = editedItem)
 
 
-# Add property
-@app.route('/category/<int:category_id>/properties/new')
-def newCategoryProperty(category_id):
-
-    return render_template('newProperty.html')
-
-# Edit property
-@app.route('/category/<int:category_id>/properties/<int:property_id>/edit')
-def editCategoryProperty(category_id, property_id):
-
-    return render_template('editProperty.html')
-
-
-# Delete property
-@app.route('/category/<int:category_id>/properties/<int:property_id>/delete')
-def deleteCategoryProperty(category_id, property_id):
-
-    return render_template('deleteProperty.html')
+# Delete item
+@app.route('/category/<int:category_id>/items/<int:item_id>/delete',methods=['GET','POST'])
+def deleteCategoryItem(category_id, item_id):
+    CatOne = session.query(Categories).filter_by(id=category_id).one()
+    itemToDelete = session.query(Items).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        flash('Menu %s Item Successfully deleted' % (itemToDelete.name))
+        return redirect(url_for('categoryItems', category_id = CatOne.id))
+    else:
+        return render_template('deleteItem.html',categ = CatOne,item =itemToDelete )
 
 
 if __name__ == '__main__':
